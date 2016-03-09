1cb706c3002456a183c747e2bca93501375eb950
Paul Mooney <mooneyp@ryanair.com>
2016-03-09 11:01:30 +0000

    Renamed core classes to reflect project

--- Aegis.Monitor.Core/AegisEventCache.cs
+++ Aegis.Monitor.Core/AegisEventCache.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Aegis.Monitor.Core
 {
-    public class EventPublisher
+    public static class AegisEventCache
     {
     }
 }
--- Aegis.Monitor.Core/AegisEventPublisher.cs
+++ Aegis.Monitor.Core/AegisEventPublisher.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Aegis.Monitor.Core
 {
-    public static class EventCache
+    public class AegisEventPublisher
     {
     }
 }
