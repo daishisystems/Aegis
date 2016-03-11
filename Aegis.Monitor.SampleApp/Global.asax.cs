9de9bfc9b58612aacf288195140ce6ff5664ca5e
Paul Mooney <mooneyp@ryanair.com>
2016-03-11 17:56:03 +0000

    Added Aegis Monitoring to test application

--- Aegis.Monitor.SampleApp/Global.asax.cs
+++ Aegis.Monitor.SampleApp/Global.asax.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Aegis.Monitor.Core;
+using FluentScheduler;
 
 namespace Aegis.Monitor.SampleApp
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -18,6 +18,28 @@ namespace Aegis.Monitor.SampleApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            #region Aegis
+
+            const string eventHubName = "samplepumper";
+            const string eventHubConnectionString =
+                "Endpoint=sb://ryanair-samplepumper.servicebus.windows.net/;SharedAccessKeyName=ALL;SharedAccessKey=ptn2jAbyjKdKEdNBoJELBzEMR84+qFo60YhQ4LvIr2I=";
+
+            try
+            {
+                // Establish connection to the Azure Event Hub
+                AegisEventPublisher.Instance.Initialise(eventHubName,
+                    eventHubConnectionString);
+
+                // Start the recurring monitor task
+                TaskManager.Initialize(new AegisRegistry());
+            }
+            catch (Exception)
+            {
+                // Fail silently and ignore errors for POC
+            }
+
+            #endregion
         }
     }
 }
