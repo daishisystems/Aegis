fdc065f5244d58ce94c38a28296a6b706b246670
Paul Mooney <mooneyp@ryanair.com>
2016-03-14 14:55:16 +0000

    Updated file and assembly versions

--- Aegis.Monitor.Core/Properties/AssemblyInfo.cs
+++ Aegis.Monitor.Core/Properties/AssemblyInfo.cs
@@ -681,11 +681,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Aegis.Monitor.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Aegis Monitoring Plugin for .NET")]
+[assembly: AssemblyDescription("Simple Aegis event-monitoring tools")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Aegis.Monitor.Core")]
+[assembly: AssemblyCompany("Paul Mooney")]
+[assembly: AssemblyProduct("Aegis Monitoring Plugin for .NET")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -708,5 +708,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
