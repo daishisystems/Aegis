2845bd7b4a66276ef6e28c89d1595b9532a8f8bb
Paul Mooney <mooneyp@ryanair.com>
2016-03-21 11:40:53 +0000

    Added Bot Monitor skeleton

---
+++ Aegis.Monitor.Logger.Bots/LoggingEventProcessor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Aegis.Monitor.Core;
+using Microsoft.ServiceBus.Messaging;
+using Nest;
+using Newtonsoft.Json;
+
+namespace Aegis.Monitor.Logger.Bots
+{
+    internal class LoggingEventProcessor : IEventProcessor
+    {
+        private readonly ConcurrentDictionary<string, string> _blacklist =
+            new ConcurrentDictionary<string, string>();
+
+        private ElasticClient _client;
+
+        private Stopwatch checkpointStopWatch;
+
+        public LoggingEventProcessor()
+        {
+            var node = new Uri("http://localhost:9200");
+            _client = new ElasticClient();
+        }
+
+        async Task IEventProcessor.CloseAsync(PartitionContext context,
+            CloseReason reason)
+        {
+            Console.WriteLine(
+                "Processor Shutting Down. Partition '{0}', Reason: '{1}'.",
+                context.Lease.PartitionId, reason);
+            if (reason == CloseReason.Shutdown)
+            {
+                await context.CheckpointAsync();
+            }
+        }
+
+        Task IEventProcessor.OpenAsync(PartitionContext context)
+        {
+            Console.WriteLine(
+                "SimpleEventProcessor initialized.  Partition: '{0}', Offset: '{1}'",
+                context.Lease.PartitionId, context.Lease.Offset);
+            checkpointStopWatch = new Stopwatch();
+            checkpointStopWatch.Start();
+            return Task.FromResult<object>(null);
+        }
+
+        async Task IEventProcessor.ProcessEventsAsync(PartitionContext context,
+            IEnumerable<EventData> messages)
+        {
+            foreach (var eventData in messages)
+            {
+                var data = Encoding.UTF8.GetString(eventData.GetBytes());
+
+                //Console.WriteLine(
+                //    "Message received.  Partition: '{0}', Data: '{1}'",
+                //    context.Lease.PartitionId, data);
+
+                var metadata = JsonConvert.DeserializeObject<AegisEvent>(data);
+
+                _blacklist.GetOrAdd(metadata.IPAddress,
+                    key =>
+                    {
+                        // todo: pump to ElasticSearch here...
+                        return "BOT";
+                    });
+
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Blacklist:");
+
+                foreach (var pair in _blacklist)
+                {
+                    Console.WriteLine(pair.Key);
+                }
+            }
+
+            //Call checkpoint every 5 minutes, so that worker can resume processing from 5 minutes back if it restarts.
+            if (checkpointStopWatch.Elapsed > TimeSpan.FromMinutes(5))
+            {
+                await context.CheckpointAsync();
+                checkpointStopWatch.Restart();
+            }
+        }
+    }
+}
---
+++ Aegis.Monitor.Logger.Bots/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Aegis.Monitor.Logger.Bots
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var eventHubConnectionString =
+                "Endpoint=sb://ryanair-bots.servicebus.windows.net/;SharedAccessKeyName=ALL;SharedAccessKey=dfSi0NS7OS3+5RRg6H2jUFAcKe2PugUCLNMWKLAlkYg=";
+            var eventHubName = "bots";
+            var storageAccountName = "ryanairbots";
+            var storageAccountKey =
+                "s44unwU9YKC4VcMPu8unmSmQViIkXRJA9kx+hADLHrnBKnZZcmvZDWlYIRjO4Q7MesbZ4Ky20j5kOa5rqcgRaQ==";
+
+            var storageConnectionString =
+                string.Format(
+                    "DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}",
+                    storageAccountName, storageAccountKey);
+
+            var eventProcessorHostName = Guid.NewGuid().ToString();
+            var eventProcessorHost =
+                new EventProcessorHost(eventProcessorHostName, eventHubName,
+                    EventHubConsumerGroup.DefaultGroupName,
+                    eventHubConnectionString, storageConnectionString);
+            Console.WriteLine("Registering EventProcessor...");
+            eventProcessorHost
+                .RegisterEventProcessorAsync<LoggingEventProcessor>
+                ().Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
