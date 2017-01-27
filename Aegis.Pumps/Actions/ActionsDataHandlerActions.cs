0877775145cf5cc525db6c524eeaa58173cddf8d
lukaszryan <lukaszryan@users.noreply.github.com>
2017-01-27 17:30:57 +0100

    Small clenaup.

--- Aegis.Pumps/Actions/ActionsDataHandlerActions.cs
+++ Aegis.Pumps/Actions/ActionsDataHandlerActions.cs
@@ -679,10 +679,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using Aegis.Core;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace Aegis.Pumps.Actions
 {
